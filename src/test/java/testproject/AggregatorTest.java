38cc40cd48b7751db441a6a66de0609a3ea7240b
otusnoctua <nazarovdo@hotmail.com>
2022-12-06 10:31:01 +0300

    aggregator reset() JUnit test added

--- src/test/java/testproject/AggregatorTest.java
+++ src/test/java/testproject/AggregatorTest.java
@@ -27,4 +27,14 @@ public class AggregatorTest {
         agr.addValue(val);
         assertThat(agr.getSum()).isEqualTo(val * 2);
     }
+
+    @Test
+    public void testReset(){
+        Aggregator agr = new Aggregator();
+        double val 15;
+        agr.addValue(val);
+        assertThat(agr.getSum()).isEqualTo(val);
+        agr.reset();
+        assertThat(agr.getSum()).isEqualTo(0);
+    }
 }
